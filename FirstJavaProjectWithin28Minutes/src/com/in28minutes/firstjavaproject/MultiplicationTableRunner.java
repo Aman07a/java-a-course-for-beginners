f301e3695c16a6208d8f4af5d81a052ecc886801
Aman07a <aman16.aa17@gmail.com>
2023-05-23 20:06:10 +0200

    Adding more methods for Multiplication Table Program

--- FirstJavaProjectWithin28Minutes/src/com/in28minutes/firstjavaproject/MultiplicationTableRunner.java
+++ FirstJavaProjectWithin28Minutes/src/com/in28minutes/firstjavaproject/MultiplicationTableRunner.java
@@ -5,5 +5,8 @@ public class MultiplicationTableRunner {
 	public static void main(String[] args) {
 		MultiplicationTable table = new MultiplicationTable();
 		table.print();
+
+		// table.print(6);
+		// table.print(6, 11, 20);
 	}
 }
