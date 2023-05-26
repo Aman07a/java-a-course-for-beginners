8353d91737553c840641db9cca96c515237b1185
Aman07a <aman16.aa17@gmail.com>
2023-05-26 08:21:06 +0200

    If Else Problem - How to get User Input in Java?

---
+++ Conditionals/src/com/in28minutes/ifstatement/examples/MenuRunner.java
@@ -0,0 +1,15 @@
+package com.in28minutes.ifstatement.examples;
+
+import java.util.Scanner;
+
+public class MenuRunner {
+
+	public static void main(String[] args) {
+		// Type obj = new Type(argument);
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter Number1: ");
+		int number1 = scanner.nextInt();
+		System.out.println("Number1 " + number1);
+	}
+
+}
