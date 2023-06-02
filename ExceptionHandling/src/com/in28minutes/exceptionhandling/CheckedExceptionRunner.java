258a75a672848caec5184b2dd1dfb06c6a7f4f07
Aman07a <aman16.aa17@gmail.com>
2023-06-02 16:36:25 +0200

    Checked Exceptions vs Unchecked Exceptions

---
+++ ExceptionHandling/src/com/in28minutes/exceptionhandling/CheckedExceptionRunner.java
@@ -0,0 +1,23 @@
+package com.in28minutes.exceptionhandling;
+
+public class CheckedExceptionRunner {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * try { someOtherMethod(); Thread.sleep(2000); } catch (Exception e) {
+		 * e.printStackTrace(); }
+		 */
+		// someOtherMethod1();
+		someOtherMethod2();
+	}
+
+	private static void someOtherMethod2() throws RuntimeException {
+
+	}
+
+	private static void someOtherMethod() throws InterruptedException {
+		Thread.sleep(2000);
+	}
+
+}
