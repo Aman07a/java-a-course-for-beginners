70c6a5aa81f151ef066b287b235d23a729f31aed
Aman07a <aman16.aa17@gmail.com>
2023-06-02 09:59:49 +0200

    Creating a Thread for Task1
    Extending Thread Class

--- ThreadsAndConcurrency/src/ThreadBasicsRunner.java
+++ ThreadsAndConcurrency/src/ThreadBasicsRunner.java
@@ -1,28 +1,71 @@
+// extends Thread
+// implements Runnable
 
-public class ThreadBasicsRunner {
+class Task1 extends Thread {
 
-	public static void main(String[] args) {
+	@Override
+	public void run() {
+		System.out.println("\nTask1 Started");
 
-		// Task1
 		for (int i = 101; i <= 199; i++) {
 			System.out.println(i + " ");
 		}
 
 		System.out.println("\nTask1 Done");
+	}
 
-		// Task2
+}
+
+class Task2 implements Runnable {
+
+	@Override
+	public void run() {
+		System.out.println("\nTask1 Started");
+
 		for (int i = 201; i <= 299; i++) {
 			System.out.println(i + " ");
 		}
 
 		System.out.println("\nTask2 Done");
+	}
 
-		// Task3
+}
+
+public class ThreadBasicsRunner {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		// • NEW;
+		// • RUNNABLE;
+		// • RUNNING;
+		// • BLOCKED/WAITING;
+		// • TERMINATED/DEAD;
+
+		// Task1 - 101 to 199
+		System.out.print("\nTask1 Kicked Off");
+		Task1 task1 = new Task1();
+		task1.setPriority(1);
+		task1.start(); // task1.run();
+
+		// Task2 - 201 to 299
+		System.out.print("\nTask2 Kicked Off");
+		Task2 task2 = new Task2();
+		Thread task2Thread = new Thread(task2);
+		task2Thread.setPriority(10);
+		task2Thread.start();
+
+		task1.join();
+		task2Thread.join();
+
+		// Task3 - 301 to 399
+		System.out.print("\nTask3 Kicked Off");
 		for (int i = 301; i <= 399; i++) {
 			System.out.println(i + " ");
 		}
 
 		System.out.println("\nTask3 Done");
+
+		System.out.print("\nMain Done");
 
 	}
 
