91cbebd3051635594289b7336cd371e86f603e68
Aman07a <aman16.aa17@gmail.com>
2023-06-02 08:16:14 +0200

    Implement Consumer Int

--- FunctionalProgramming/src/com/in28minutes/functionalprogramming/LambdaBehindTheScreensRunner.java
+++ FunctionalProgramming/src/com/in28minutes/functionalprogramming/LambdaBehindTheScreensRunner.java
@@ -1,6 +1,7 @@
 package com.in28minutes.functionalprogramming;
 
 import java.util.List;
+import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 class EvenNumberPredicate implements Predicate<Integer> {
@@ -12,16 +13,30 @@ class EvenNumberPredicate implements Predicate<Integer> {
 
 }
 
+class SystemOutConsumer implements Consumer<Integer> {
+
+	@Override
+	public void accept(Integer number) {
+		System.out.println(number);
+	}
+
+}
+
 public class LambdaBehindTheScreensRunner {
 
 	public static void main(String[] args) {
 
 		List.of(23, 43, 34, 45, 36, 48).stream().filter(n -> n % 2 == 0).forEach(e -> System.out.println(e));
 
-		List.of(23, 43, 34, 45, 36, 48).stream().filter(new EvenNumberPredicate()).forEach(e -> System.out.println(e));
+		List.of(23, 43, 34, 45, 36, 48).stream().filter(new EvenNumberPredicate()).forEach(new SystemOutConsumer());
 
+		// .filter(new EvenNumberPredicate())
 		// Stream<T> filter(Predicate<? super T> predicate);
 		// boolean test(T t);
+
+		// .forEach(e -> System.out.println(e));
+		// Consumer<? super T> action
+		// void accept(T t);
 
 	}
 
