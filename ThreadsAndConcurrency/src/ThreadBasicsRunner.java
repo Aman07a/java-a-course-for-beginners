437aecf364dc862f1895f95ec7a3617b0a2b2358
Aman07a <aman16.aa17@gmail.com>
2023-06-02 09:46:42 +0200

    Introduction to Threads and MultiThreading
    Need for Threads

---
+++ ThreadsAndConcurrency/src/ThreadBasicsRunner.java
@@ -0,0 +1,29 @@
+
+public class ThreadBasicsRunner {
+
+	public static void main(String[] args) {
+
+		// Task1
+		for (int i = 101; i <= 199; i++) {
+			System.out.println(i + " ");
+		}
+
+		System.out.println("\nTask1 Done");
+
+		// Task2
+		for (int i = 201; i <= 299; i++) {
+			System.out.println(i + " ");
+		}
+
+		System.out.println("\nTask2 Done");
+
+		// Task3
+		for (int i = 301; i <= 399; i++) {
+			System.out.println(i + " ");
+		}
+
+		System.out.println("\nTask3 Done");
+
+	}
+
+}
