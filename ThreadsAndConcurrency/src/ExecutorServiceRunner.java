f86c0ec99498e4287977f127499c5b7c4c2c370d
Aman07a <aman16.aa17@gmail.com>
2023-06-02 10:46:44 +0200

    Executor Service
    Customizing number of Threads

--- ThreadsAndConcurrency/src/ExecutorServiceRunner.java
+++ ThreadsAndConcurrency/src/ExecutorServiceRunner.java
@@ -1,26 +1,41 @@
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+class Task extends Thread {
+
+	private int number;
+
+	public Task(int number) {
+		this.number = number;
+	}
+
+	@Override
+	public void run() {
+		System.out.print("\nTask" + number + " Started ");
+
+		for (int i = number * 100; i <= number * 100 + 99; i++) {
+			System.out.print(i + " ");
+		}
+
+		System.out.print("\nTask" + number + " Done ");
+	}
+
+}
+
 public class ExecutorServiceRunner {
 
 	public static void main(String[] args) {
 
-		ExecutorService executorService = Executors.newSingleThreadExecutor();
+		// ExecutorService executorService = Executors.newSingleThreadExecutor();
+		ExecutorService executorService = Executors.newFixedThreadPool(5);
 
-		executorService.execute(new Task1());
-
-		executorService.execute(new Thread(new Task2()));
-
-		System.out.print("\nTask3 Kicked Off");
-
-		// Task3 - 301 to 399
-		for (int i = 301; i <= 399; i++) {
-			System.out.println(i + " ");
-		}
-
-		System.out.println("\nTask3 Done");
-
-		System.out.print("\nMain Done");
+		executorService.execute(new Task(1));
+		executorService.execute(new Task(2));
+		executorService.execute(new Task(3));
+		executorService.execute(new Task(4));
+		executorService.execute(new Task(5));
+		executorService.execute(new Task(6));
+		executorService.execute(new Task(7));
 
 		executorService.shutdown();
 
