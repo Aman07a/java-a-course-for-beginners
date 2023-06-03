b0a83d37cb6b727701495af2f879897da88ec503
Aman07a <aman16.aa17@gmail.com>
2023-06-03 18:40:57 +0200

    Static methods cannot use instance methods or variables

--- tips-and-tricks/src/com/in28minutes/tips/access/package1/StaticModifierRunner.java
+++ tips-and-tricks/src/com/in28minutes/tips/access/package1/StaticModifierRunner.java
@@ -2,6 +2,7 @@ package com.in28minutes.tips.access.package1;
 
 class Player {
 	private String name;
+
 	private static int count = 0;
 
 	public Player(String name) {
@@ -12,6 +13,15 @@ class Player {
 
 	static public int getCount() {
 		return count;
+	}
+
+	public String getName() {
+		System.out.println(count);
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
 	}
 
 }
