5664e790b1931f3cb6d7c2ecd0c9c73c1b6800fd
Aman07a <aman16.aa17@gmail.com>
2023-06-04 09:03:09 +0200

    Quick look at inbuild Enums - Month, DayOfWeek

--- tips-and-tricks/src/com/in28minutes/tips/enums/EnumRunner.java
+++ tips-and-tricks/src/com/in28minutes/tips/enums/EnumRunner.java
@@ -2,22 +2,6 @@ package com.in28minutes.tips.enums;
 
 import java.util.Arrays;
 
-enum Season {
-	SPRING(4), SUMMER(1), WINTER(2), FALL(3);
-
-	private int value;
-
-	private Season(int value) {
-		this.value = value;
-	}
-
-	public int getValue() {
-		return value;
-	}
-
-	// 0,1,2,3
-}
-
 public class EnumRunner {
 
 	public static void main(String[] args) {
