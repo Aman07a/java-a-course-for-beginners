338e99026bcf32cd1a82ae1f90feffeaeeafc53d
Aman07a <aman16.aa17@gmail.com>
2023-06-05 10:12:38 +0200

    Different Types of Dependency Injection

--- learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework02/game/SuperContraGame.java
+++ learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework02/game/SuperContraGame.java
@@ -1,21 +1,30 @@
 package com.in28minutes.learnspringframework02.game;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+@Qualifier("SuperContraGameQualifier")
 public class SuperContraGame implements GamingConsole {
 
+	@Override
 	public void up() {
-		System.out.println("up");
+		System.out.println("Super Contra: Up");
 	}
 
+	@Override
 	public void down() {
-		System.out.println("Sit down");
+		System.out.println("Super Contra: Down");
 	}
 
+	@Override
 	public void left() {
-		System.out.println("Go back");
+		System.out.println("Super Contra: Left");
 	}
 
+	@Override
 	public void right() {
-		System.out.println("Shoot a bullet");
+		System.out.println("Super Contra: Right");
 	}
 
 }
