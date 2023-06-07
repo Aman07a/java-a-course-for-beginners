62389cbbf0a353843c880fe6427b6180caf51ec1
Aman07a <aman16.aa17@gmail.com>
2023-06-07 15:34:11 +0200

    Building REST API to update a course - PUT

--- learn-spring-boot/src/main/java/com/in28minutes/learnspringboot/courses/controller/CourseController.java
+++ learn-spring-boot/src/main/java/com/in28minutes/learnspringboot/courses/controller/CourseController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -50,6 +51,11 @@ public class CourseController {
 	}
 
 	// PUT - Update/Replace a resource (/courses/1)
+	@PutMapping("/courses/{id}")
+	public void updateCourse(@PathVariable long id, @RequestBody Course course) {
+		repository.save(course);
+	}
+
 	// DELETE - Delete a resource (/courses/1)
 
 }
