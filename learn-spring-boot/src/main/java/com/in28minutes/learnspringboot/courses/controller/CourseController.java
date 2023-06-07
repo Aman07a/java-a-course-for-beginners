0586f619c03961df6256c7246e29f30ea641a9b3
Aman07a <aman16.aa17@gmail.com>
2023-06-07 10:15:12 +0200

    Exploring Spring Boot DevTools

--- learn-spring-boot/src/main/java/com/in28minutes/learnspringboot/courses/controller/CourseController.java
+++ learn-spring-boot/src/main/java/com/in28minutes/learnspringboot/courses/controller/CourseController.java
@@ -22,7 +22,7 @@ public class CourseController {
 	// http://localhost:8080/courses/1
 	@GetMapping("/courses/1")
 	public Course getCourseDetails() {
-		return new Course(1, "Learn Microservices", "in28minutes");
+		return new Course(1, "Learn Microservices 1", "in28minutes");
 	}
 
 }
