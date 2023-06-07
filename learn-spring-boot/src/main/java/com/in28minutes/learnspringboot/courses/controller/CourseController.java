35d07492ac97dd9433afffd83fb1f347eef9f7fe
Aman07a <aman16.aa17@gmail.com>
2023-06-07 15:31:53 +0200

    Building REST API to retrieve details of a course - GET

--- learn-spring-boot/src/main/java/com/in28minutes/learnspringboot/courses/controller/CourseController.java
+++ learn-spring-boot/src/main/java/com/in28minutes/learnspringboot/courses/controller/CourseController.java
@@ -1,9 +1,11 @@
 package com.in28minutes.learnspringboot.courses.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.in28minutes.learnspringboot.courses.bean.Course;
@@ -23,8 +25,20 @@ public class CourseController {
 
 	// http://localhost:8080/courses/1
 	@GetMapping("/courses/1")
-	public Course getCourseDetails() {
-		return new Course(1, "Learn Microservices 1", "in28minutes");
+	public Course getCourseDetails(@PathVariable long id) {
+
+		Optional<Course> course = repository.findById(id);
+
+		if (course.isEmpty()) {
+			throw new RuntimeException("Course not found with id " + id);
+		}
+
+		return course.get();
 	}
+
+	// GET - Retrieve information (/courses, /courses/1)
+	// POST - Create a new resource (/courses)
+	// PUT - Update/Replace a resource (/courses/1)
+	// DELETE - Delete a resource (/courses/1)
 
 }
